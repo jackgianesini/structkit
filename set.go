59edeb384918e9632b4fb8a4a1023b4a0be1dfe8
lab210-dev <19929065+lab210-dev@users.noreply.github.com>
2023-04-15 23:33:18 -0400

    :construction: First try

---
+++ set.go
@@ -0,0 +1,83 @@
+package structkit
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type set struct {
+	source     reflect.Value
+	kind       reflect.Kind
+	value      any
+	fieldSplit []string
+}
+
+func Set(source any, field string, value any) (err error) {
+	set := new(set)
+
+	set.source = reflect.ValueOf(source)
+	if !set.source.IsValid() {
+		return errors.New("source is invalid")
+	}
+
+	if set.source.Kind() != reflect.Ptr {
+		return errors.New("source must be a pointer")
+	}
+
+	if set.source.IsNil() {
+		return errors.New("source is nil")
+	}
+
+	set.source = set.source.Elem()
+	set.kind = set.source.Kind()
+	set.fieldSplit = strings.Split(field, ".")
+	set.value = value
+
+	if set.kind != reflect.Struct && set.kind != reflect.Slice {
+		return errors.New("source must be a struct or a slice of struct")
+	}
+
+	return set.apply()
+}
+
+func (set *set) apply() (err error) {
+	/*	if set.kind == reflect.Slice {
+		return set.applyToSlice()
+	}*/
+	return set.applyToStruct()
+}
+
+/*func (set *set) applyToSlice() (err error) {
+	return
+}*/
+
+func (set *set) workingFieldName() string {
+	return set.fieldSplit[0]
+}
+
+func (set *set) lenFieldSplit() int {
+	return len(set.fieldSplit)
+}
+
+func (set *set) applyToStruct() (err error) {
+	if set.lenFieldSplit() > 1 {
+		return errors.New("not implemented yet")
+	}
+
+	workingField := set.source.FieldByName(set.workingFieldName())
+	if !workingField.IsValid() {
+		return fmt.Errorf("field %s not found", set.workingFieldName())
+	}
+
+	value := reflect.ValueOf(set.value)
+
+	if workingField.Kind() != value.Kind() {
+		return errors.New("type mismatch")
+	}
+
+	workingField.Set(value)
+
+	return
+}
---
+++ set_test.go
@@ -0,0 +1,79 @@
+package structkit
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type PostSet struct {
+	Title    string
+	TitlePtr *string
+	Creator  UserSet
+	Editor   *User
+	Comments []CommentSet
+}
+
+type UserSet struct {
+	Name string
+
+	unexportedField bool
+}
+
+type CommentSet struct {
+	Content string
+}
+
+type SetTestSuite struct {
+	suite.Suite
+}
+
+func (test *SetTestSuite) TestSetErr() {
+	// nil
+	err := Set(nil, "Title", "Post Title")
+	test.Error(err)
+
+	// nil pointer
+	nilPayload := new(PostSet)
+	nilPayload = nil
+	err = Set(nilPayload, "Title", "Post Title")
+	test.Error(err)
+
+	// not a pointer
+	err = Set(PostSet{}, "Title", "Post Title")
+	test.Error(err)
+
+	// not a slice or struct
+	err = Set(new(int), "Title", "Post Title")
+	test.Error(err)
+
+	buildCache := PostSet{Creator: UserSet{Name: "John Doe", unexportedField: false}}
+	err = Set(&buildCache, "Creator.Name", "John Doe")
+	test.Error(err)
+
+	err = Set(&buildCache, "unknown", "Post Title")
+	test.Error(err)
+
+	err = Set(&buildCache, "TitlePtr", "Post Title")
+	test.Error(err)
+}
+
+func (test *SetTestSuite) TestSet() {
+	buildCache := PostSet{}
+
+	expected := "Post Title"
+
+	err := Set(&buildCache, "Title", "Post Title")
+	test.NoError(err)
+	test.Equal(expected, buildCache.Title)
+
+	titlePtr := new(string)
+	*titlePtr = "Post Title"
+
+	err = Set(&buildCache, "TitlePtr", titlePtr)
+	test.NoError(err)
+	test.Equal(titlePtr, buildCache.TitlePtr)
+}
+
+func TestSetTestSuite(t *testing.T) {
+	suite.Run(t, new(SetTestSuite))
+}
